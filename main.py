7ecb1c7e78174bc931a836a41d1d7a130c3f0e7b
heritierbagumire <heritierbagumire367@gmail.com>
2024-04-27 22:53:39 +0200

    final commit

--- main.py
+++ main.py
@@ -4,12 +4,4 @@ import fiftyone.zoo as foz
 dataset = foz.load_zoo_dataset("quickstart")
 session = fo.launch_app(dataset, port=5151)
 
-
-
-
-
-dataset2 = fo.load_zoo_dataset("animals")
-session = fo.launch_app(dataset2, port=5052)
-
-
 session.wait()
